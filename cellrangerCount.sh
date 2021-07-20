5f7927509dfd4e57c983e7900fe9c690f0ff3213
Alex Smith <alexander.smith@mssm.edu>
2021-07-20 09:14:10 -0400

    Decreased default localcores

--- cellrangerCount.sh
+++ cellrangerCount.sh
@@ -2,6 +2,6 @@ cellranger count --id=DP_OC3 \
 --transcriptome=/d1/software/cellranger/refdata-gex-mm10-2020-A \
 --fastqs=/d2/studies/scanPy/ACWS_DP/RawData/930_3GEX_DP_OC3_premrna/fastqs/930_3GEX_DP_DC3-mouse-nuclei-premrna_0_1_HVLLMDSXX/ \
 --sample=930_3GEX_DP_OC3 \
---localcores=18 \
+--localcores=16 \
 --localmem=128 \
 --include-introns
