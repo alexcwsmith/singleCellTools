611fb9ce14878d41db0ec14618a39417f59df909
Alex Smith <alexander.smith@mssm.edu>
2021-07-20 09:17:01 -0400

    Decreased default localcores

--- cellrangerAggr.sh
+++ cellrangerAggr.sh
@@ -1,6 +1,6 @@
 cellranger aggr --id=DP_OC2_3_Merged_NoNorm \
 --csv=/d2/studies/scanPy/ACWS_DP/RawData/OC_AggrIndex.csv \
 --normalize=none \
---localcores=18 \
+--localcores=12 \
 --localmem=128 \
 --nosecondary
